900c6873e9dd3761fa548fae9584c087b3a76c0e
Carlos Carbajal <carloscarbajalrojas@hotmail.com>
2020-08-03 12:34:23 -0500

    Aplicacion Ventas: Generando

--- src/java/Modelo/Productos.java
+++ src/java/Modelo/Productos.java
@@ -10,5 +10,65 @@ package Modelo;
  * @author TuNombre
  */
 public class Productos {
+    private int idproducto;
+    private String nombre;
+    private String precio;
+    private String estado;
+
+    /**
+     * @return the idproducto
+     */
+    public int getIdproducto() {
+        return idproducto;
+    }
+
+    /**
+     * @param idproducto the idproducto to set
+     */
+    public void setIdproducto(int idproducto) {
+        this.idproducto = idproducto;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the precio
+     */
+    public String getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
     
 }
